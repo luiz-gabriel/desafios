5b8375f3386f2d6005f967166e3c711d9a0f6775
luiz gabriel <luizgabriel515@gmail.com>
2023-07-16 22:52:33 +0000

    Created limit checking and limit definition

--- ubarn-move-estagio/RealEstateLinkScraping.php
+++ ubarn-move-estagio/RealEstateLinkScraping.php
@@ -1,11 +1,15 @@
 <?php
 
     require './ScrapingProperties.php';
+    //Arquivo de configuração para aumentar o limite de memória e tempo do php
+    require './config.php';
     class RealEstateLinkScraping{
         private $domDocument;
         private $domXPath;
         private String $contentPage;
         private array $data = [];
+        private $index = 1;
+        private $limit = 0;
         public function __construct(private String $linkStart){
             $this->grabContentPageAndSaveContent($this->linkStart);
             $this->initializeDomDocumentClassAndLoadHtml();
@@ -23,7 +27,11 @@
             $this->domXPath = new DOMXPath($this->domDocument);
         }
         private function grabContentPageAndSaveContent(String $propertyLinkToScraping){
-            $this->contentPage = file_get_contents($propertyLinkToScraping);
+            try{
+                $this->contentPage = file_get_contents($propertyLinkToScraping);
+            }catch(Error $getContentError){
+                print_r($getContentError);
+            }
         }
         private function loadHTML(){
             $this->domDocument->loadHTML($this->contentPage);
@@ -36,12 +44,21 @@
                 $ScrapingProperties = new ScrapingProperties($link);
                 array_push($this->data, $ScrapingProperties->getDataToArray());
             }
-            if(!empty($this->getNextPageLink())){
+
+            /*
+             * É recomendado definir um limite de páginas, pois o tempo para terminar pode ser longo e pode chegar a horas.
+            */
+            if(!empty($this->getNextPageLink()) && $this->index <= $this->limit || !empty($this->getNextPageLink()) && $this->limit == 0){
                 $this->nextPage($this->getNextPageLink());
+                $this->index++;
                 $this->scrapping();
             }
             
         }
+        public function setLimit(int $limit){
+            $this->limit = $limit;
+        }
+
         //Captura todos os links de uma página
         public function getAllLinkOfRealState(){
 
@@ -74,6 +91,8 @@
 
     }
     $scrapping = new RealEstateLinkScraping('https://agostinholeiloes.com.br/lotes/imovel?tipo=imovel&categoria_id=1&data_leilao_ini=&data_leilao_fim=&lance_inicial_ini=&lance_inicial_fim=&address_uf=&address_cidade_ibge=&address_logradouro=&comitente_id=&search=&page=1');
-
+    //Definindo o limite de páginas para páginar.
+    $scrapping->setLimit(3);
     $scrapping->scrapping();
     $scrapping->showData();
+    
